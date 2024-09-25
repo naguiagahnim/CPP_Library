fac6ed4064bc7da346030401b40e64ce81e2c9e5
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2024-09-25 11:52:00 +0200

    While implémenté Reste le module

--- main.cpp
+++ main.cpp
@@ -20,68 +20,78 @@ int main() {
 
 
     int choix;
-    cout<<"Que voulez-vous faire ?"<<endl;
-    cout<<"1. Afficher l'ensemble des livres de la bibliotheque"<<endl;
-    cout<<"2.Ajouter un livre à la bibliotheque"<<endl;
-    cout<<"3. Supprimer un livre de la bibliotheque"<<endl;
-    cout<<"4.Rechercher un livre par son titre"<<endl;
-    cout<<"5. Quitter"<<endl;
-    cin>>choix;
+    do {
+        cout<<"Que voulez-vous faire ?"<<endl;
+        cout<<"1. Afficher l'ensemble des livres de la bibliotheque"<<endl;
+        cout<<"2.Ajouter un livre à la bibliotheque"<<endl;
+        cout<<"3. Supprimer un livre de la bibliotheque"<<endl;
+        cout<<"4.Rechercher un livre par son titre"<<endl;
+        cout<<"5. Quitter"<<endl;
+        cin>>choix;
 
-    if(choix==1) {
-        for(int i=0;i<sizeof(bibliotheque) / sizeof(bibliotheque[0]);i++) {
-            cout<<"------------------------------------------------------"<<endl;
-            bibliotheque[i].Affiche();
-        }
-    }
-    else if(choix==2) {
-        string nvTitre;
-        string nvNom;
-        int nvA;
-        int nvNb;
-        cout<<"Entrez le titre du livre : ";
-        cin>>nvTitre;
-        cout<<"Entrez le nom de l'auteur : ";
-        cin>>nvNom;
-        cout<<"Entrez l'annee de publication : ";
-        cin>>nvA;
-        cout<<"Entrez le nombre de pages : ";
-        cin>>nvNb;
-        Livre nvLivre;
-        nvLivre.setLivre(nvTitre, nvNom, nvA, nvNb);
-        if (bibliotheque_len < 100) {
-            bibliotheque[bibliotheque_len++] = nvLivre;
-            bibliotheque_len++;
-        } else {
-            cout<<"La bibliothèque est pleine"<<endl;
-            return -1;
-        }
-    }
-    else if(choix==3) {
-        //Supprimer un livre
-        string titre;
-        cout<<"Entrez le titre du livre à supprimer : ";
-        cin>>titre;
-        for(int i=0;i<sizeof(bibliotheque) / sizeof(bibliotheque[0]);i++) {
-            if(bibliotheque[i].getTitre()==titre) {
-                for(int j=i;j<sizeof(bibliotheque) / sizeof(bibliotheque[0])-1;j++) {
-                    bibliotheque[j] = bibliotheque[j+1];
-                }
-                bibliotheque_len--;
-                break;
+        if(choix==1) {
+            for(int i=0;i<sizeof(bibliotheque) / sizeof(bibliotheque[0]);i++) {
+                cout<<"------------------------------------------------------"<<endl;
+                bibliotheque[i].Affiche();
             }
         }
-    }
-    else if(choix==4) {
+        else if(choix==2) {
+            string nvTitre;
+            string nvNom;
+            int nvA;
+            int nvNb;
+            cout<<"Entrez le titre du livre : ";
+            cin>>nvTitre;
+            cout<<"Entrez le nom de l'auteur : ";
+            cin>>nvNom;
+            cout<<"Entrez l'annee de publication : ";
+            cin>>nvA;
+            cout<<"Entrez le nombre de pages : ";
+            cin>>nvNb;
+            Livre nvLivre;
+            nvLivre.setLivre(nvTitre, nvNom, nvA, nvNb);
+            if (bibliotheque_len < 100) {
+                bibliotheque[bibliotheque_len++] = nvLivre;
+                bibliotheque_len++;
+            } else {
+                cout<<"La bibliothèque est pleine"<<endl;
+                return -1;
+            }
+        }
+        else if(choix==3) {
+            //Supprimer un livre
+            string titre;
+            cout<<"Entrez le titre du livre à supprimer : ";
+            cin>>titre;
+            for(int i=0;i<sizeof(bibliotheque) / sizeof(bibliotheque[0]);i++) {
+                if(bibliotheque[i].getTitre()==titre) {
+                    for(int j=i;j<sizeof(bibliotheque) / sizeof(bibliotheque[0])-1;j++) {
+                        bibliotheque[j] = bibliotheque[j+1];
+                    }
+                    bibliotheque_len--;
+                    break;
+                }
+            }
+        }
+        else if(choix==4) {
+            //Rechercher un livre
+            string titre;
+            cout<<"Entrez le titre du livre à rechercher : ";
+            cin>>titre;
+            for(int i=0;i<sizeof(bibliotheque) / sizeof(bibliotheque[0]);i++) {
+                if(bibliotheque[i].getTitre()==titre) {
+                    bibliotheque[i].Affiche();
+                    break;
+                }
+            }
+        }
+        else if(choix==5) {
+            cout<<"Au revoir !"<<endl;
+        }
+        else {
+            cout<<"Choix invalide"<<endl;
+        }
+    } while(choix != 5);
 
+        return 0;
     }
-    else if(choix==5) {
-        cout<<"Au revoir !"<<endl;
-    }
-    else {
-        cout<<"Choix invalide"<<endl;
-        return -1;
-    }
-
-    return 0;
-}
